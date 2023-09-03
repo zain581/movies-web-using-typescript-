a588e6decfdb41dc4463283258052389332037b2
zain581 <az4860364@gmail.com>
2023-09-03 08:53:36 -0400

    home file update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import MoviesList from '../MoviesList/MoviesList';
 //home const
+//test funct in test module
 const Home = () => {
     return (
         <div>
